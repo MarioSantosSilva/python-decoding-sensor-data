99f6d78cab16e65f78e6f5db93ecd015f610a574
MarioSantosSilva <mario.santos.silva@celfocus.com>
2022-02-06 00:50:03 +0000

    Update house_info.py

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -2,5 +2,6 @@ class HouseInfo():
     def HouseInfo(self, data):
         data = self.data
     
-    def get_data_by_area(self, datasets.field, rec_area=0):
+    def get_data_by_area(self, field, rec_area):
+        rec_area = 0
         field_data=[]
