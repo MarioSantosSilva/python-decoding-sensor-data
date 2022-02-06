40346232a85185600e296fbbab6379400eabd106
MarioSantosSilva <mario.santos.silva@celfocus.com>
2022-02-06 02:11:50 +0000

    Update humidity_info.py

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -1,4 +1,5 @@
 from house_info import HouseInfo 
+from datetime import date, datetime
 
 class HumidityData (HouseInfo):
     
@@ -10,4 +11,8 @@ class HumidityData (HouseInfo):
     
     def get_data_by_area(self, rec_area=0):
         recs = super().get_data_by_area("humidity", rec_area)
+        return self.convert_data(recs)
+    
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("humidity", rec_date)
         return self.convert_data(recs)
