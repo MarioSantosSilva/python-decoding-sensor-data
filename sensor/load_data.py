f3244f743cc8e6cf91adf477426cf2aa1ff0b9e7
MarioSantosSilva <mario.santos.silva@celfocus.com>
2022-02-06 00:19:37 +0000

    Update load_data.py

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -7,6 +7,5 @@ def load_sensor_data():
 
 sensor_files=glob.glob(os.path.join(os.getcwd(), "datasets", "*csv"))
 for sensor_file in sensor_files:
-    open(sensor_file) with data_file
-    data_reader = csv.DictReader(data_file, delimiter=',')
-    
+    with open(sensor_file) as data_file:
+        data_reader = csv.DictReader(data_file, delimiter=',')
