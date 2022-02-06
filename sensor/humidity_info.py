2087b6e1617c674fd423741c6e549ff8c4ad044a
MarioSantosSilva <mario.santos.silva@celfocus.com>
2022-02-06 02:08:29 +0000

    Update humidity_info.py

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -7,3 +7,7 @@ class HumidityData (HouseInfo):
         for rec in data: 
             recs.append(float(rec)*100)
         return recs 
+    
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("humidity", rec_area)
+        return self.convert_data(recs)
