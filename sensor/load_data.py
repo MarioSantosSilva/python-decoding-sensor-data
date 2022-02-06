0c35f9f8f23c9a27f5a5a99df85bbdf6443ea5b5
MarioSantosSilva <mario.santos.silva@celfocus.com>
2022-02-06 00:29:30 +0000

    Update load_data.py

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -9,3 +9,6 @@ def load_sensor_data():
     for sensor_file in sensor_files:
         with open(sensor_file) as data_file:
             data_reader = csv.DictReader(data_file, delimiter=',')
+            for row in data_reader:
+            sensor.data.append(row)
+    return sensor_data
