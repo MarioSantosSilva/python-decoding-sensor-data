14a06bc4a0f79e0b35ae30ea5c0de27b88553307
MarioSantosSilva <mario.santos.silva@celfocus.com>
2022-02-06 02:05:43 +0000

    Update humidity_info.py

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -1,6 +1,9 @@
 from house_info import HouseInfo 
 
-class HumidityDate (HouseInfo):
+class HumidityData (HouseInfo):
     
     def _convert_data(self, data):
         recs=[]
+        for rec in data: 
+            recs.append(float(rec)*100)
+        return recs 
